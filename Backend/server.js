bc4082bc3e9184d24f4334f4eae3c9c7c01ce573
Santosh Kumar Gupta <73736582+Santosh5358@users.noreply.github.com>
2025-10-16 15:40:55 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 
 require('dotenv').config();
 // MongoDB Connection
-onst mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 // Connect to MongoDB using the URI from environment variables
 mongoose.connect(process.env.MONGODB_URI, {
